2dc747958711f966229f304bd529c27c7e738c8b
Nishant Tyagi <ntyagi893@gmail.com>
2022-02-26 18:00:51 +0530

    Authentication using JDBC

--- src/main/java/com/example/springsecuritymethods/SecurityConfig.java
+++ src/main/java/com/example/springsecuritymethods/SecurityConfig.java
@@ -1,19 +1,32 @@
 package com.example.springsecuritymethods;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import javax.sql.DataSource;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private DataSource dataSource;
+
+    @Autowired
+    private BCryptPasswordEncoder passwordEncoder;
+
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        // {noop} => No operation for password encoder	(no password encoding needed)
-        auth.inMemoryAuthentication().withUser("emp").password("{noop}password").authorities("EMPLOYEE");
-        auth.inMemoryAuthentication().withUser("admin").password("{noop}password").authorities("ADMIN");
-        auth.inMemoryAuthentication().withUser("mgr").password("{noop}password").authorities("MANAGER");
+        auth.jdbcAuthentication()
+                .dataSource(dataSource)
+                .usersByUsernameQuery("select user_name,user_pwd,user_enabled from user where user_name=?")
+                .authoritiesByUsernameQuery("select user_name,user_role from user where user_name=?")
+                .passwordEncoder(passwordEncoder);
     }
 
     @Override
@@ -24,7 +37,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/admin").hasAuthority("ADMIN")
                 .antMatchers("/emp").hasAuthority("EMPLOYEE")
                 .antMatchers("/mgr").hasAuthority("MANAGER")
-                .antMatchers("/common").hasAnyAuthority("ADMIN", "EMPLOYEE","MANAGER")
 
                 // Any other URLs which are not configured in above antMatchers
                 // generally declared aunthenticated() in real time
